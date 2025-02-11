eb1ac640d2d5b14bd3c3fabcf2f89bd76150adf3
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-11 08:40:30 -0500

    Initial Commit

---
+++ ParkSmart/ParkSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkSmartApp.swift
+//  ParkSmart
+//
+//  Created by Mihiretu Jackson on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ParkSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
